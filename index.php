f71e44b39c679a0e9c4e2de9bd44a54971f3d64a
accbuatdb <42600102+accbuatdb@users.noreply.github.com>
2018-08-22 14:22:46 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+/*
+copyright @ medantechno.com
+2017
+
+*/
+
+require_once('./line_class.php');
+
+$channelAccessToken = 'YOUR-CHANNEL-ACCESS-TOKEN'; //sesuaikan 
+$channelSecret = 'YOUR-CHANNEL-SECRET-CODE';//sesuaikan
+
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+$userId 	= $client->parseEvents()[0]['source']['userId'];
+$replyToken = $client->parseEvents()[0]['replyToken'];
+$timestamp	= $client->parseEvents()[0]['timestamp'];
+$message 	= $client->parseEvents()[0]['message'];
+$messageid 	= $client->parseEvents()[0]['message']['id'];
+$profil = $client->profil($userId);
+$pesan_datang = $message['text'];
+
+//pesan bergambar
+if($message['type']=='text')
+{
+	if($pesan_datang=='Hi')
+	{
+		
+		
+		$balas = array(
+							'replyToken' => $replyToken,														
+							'messages' => array(
+								array(
+										'type' => 'text',					
+										'text' => 'Halo'
+									)
+							)
+						);
+				
+	}
+
+}
+ 
+$result =  json_encode($balas);
+//$result = ob_get_clean();
+
+file_put_contents('./balasan.json',$result);
+
+
+$client->replyMessage($balas);
+
+?>
